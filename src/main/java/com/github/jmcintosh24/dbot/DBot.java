848ee21d0600d77e184f515d2e8be8b14d12f391
BuildTools <unconfigured@null.spigotmc.org>
2022-08-02 12:08:54 -0400

    Implemented time joined feature. Allows users to find out the exact date a member joined their server. Also added try-catch blocks for NullPointerExceptions.

--- src/main/java/com/github/jmcintosh24/dbot/DBot.java
+++ src/main/java/com/github/jmcintosh24/dbot/DBot.java
@@ -9,6 +9,7 @@ package com.github.jmcintosh24.dbot;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
@@ -16,7 +17,10 @@ import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
 
 import javax.security.auth.login.LoginException;
+import java.time.OffsetDateTime;
 import java.util.EnumSet;
+
+import static net.dv8tion.jda.api.interactions.commands.OptionType.*;
 
 public class DBot extends ListenerAdapter {
     public static void main(String[] args) throws LoginException {
@@ -30,9 +34,11 @@ public class DBot extends ListenerAdapter {
         CommandListUpdateAction commands = jda.updateCommands();
 
         commands.addCommands(
-                Commands.slash("help", "Display a list of all commands"),
-                Commands.slash("ping", "Get the ping of the bot in ms"),
-                Commands.slash("count", "Get the total number of members in this server")
+                Commands.slash("help", "Display a list of all commands."),
+                Commands.slash("ping", "Get the ping of the bot in ms."),
+                Commands.slash("count", "Get the total number of members in this server."),
+                Commands.slash("jointime", "Get the date that a member joined this server.")
+                        .addOption(USER, "user", "The user to get the date info on.", true)
         );
 
         commands.queue();
@@ -40,7 +46,7 @@ public class DBot extends ListenerAdapter {
 
     @Override
     public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
-        if(event.getGuild() != null) {
+        if (event.getGuild() != null) {
             switch (event.getName()) {
                 case "help":
                     help(event);
@@ -51,6 +57,9 @@ public class DBot extends ListenerAdapter {
                 case "count":
                     count(event);
                     break;
+                case "jointime":
+                    jointime(event);
+                    break;
                 default:
                     event.reply("That is not a valid command :(").setEphemeral(true).queue();
             }
@@ -59,10 +68,11 @@ public class DBot extends ListenerAdapter {
 
     public void help(SlashCommandInteractionEvent event) {
         event.reply("""
-                        /help - Display a list of all commands
-                        /ping - Get the ping of the bot in ms
-                        /count - Get the total number of members in this server"""
-        ).setEphemeral(true)
+                        /help - Display a list of all commands.
+                        /ping - Get the ping of the bot in ms.
+                        /count - Get the total number of members in this server.
+                        /jointime [member] - Get the date that a member joined this server."""
+                ).setEphemeral(true)
                 .queue();
     }
 
@@ -76,8 +86,39 @@ public class DBot extends ListenerAdapter {
     }
 
     public void count(SlashCommandInteractionEvent event) {
-        event.reply("There are " +event.getGuild().getMemberCount()+ " members in this server.")
-                .setEphemeral(true)
-                .queue();
+        try {
+            event.reply("There are " + event.getGuild().getMemberCount() + " members in this server.")
+                    .setEphemeral(true)
+                    .queue();
+        } catch (NullPointerException e) {
+            event.reply("There was an error getting the member count.")
+                    .setEphemeral(true)
+                    .queue();
+        }
+    }
+
+    public void jointime(SlashCommandInteractionEvent event) {
+        try {
+            Member user = event.getOption("user").getAsMember();
+            //If the user's join date is properly recorded, then carry out the date retrieval
+            if (user.hasTimeJoined()) {
+                OffsetDateTime timeJoined = user.getTimeJoined();
+
+                int day = timeJoined.getDayOfMonth();
+                String month = timeJoined.getMonth().toString();
+                int year = timeJoined.getYear();
+
+                event.reply(user.getEffectiveName() + " joined " + event.getGuild().getName() + " on " + month +
+                                " " + day + ", " + year)
+                        .setEphemeral(true)
+                        .queue();
+            } else {
+                event.reply("Sorry, the date info on " + user.getEffectiveName() + "is not available :(");
+            }
+        } catch (IllegalStateException | NullPointerException e) {
+            event.reply("There was an error finding information on the given user :(")
+                    .setEphemeral(true)
+                    .queue();
+        }
     }
 }
